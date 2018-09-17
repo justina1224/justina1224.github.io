5df2f013c6aad48cf15ca7d1a739311366565136
justina1224 <justyna.pilzno@gmail.com>
2018-09-17 17:42:44 +0200

    Initial commit 2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'https://enigmatic-retreat-39554.herokuapp.com/v1/task/';
+  var apiRoot = 'https://enigmatic-retreat-39554.herokuapp.com//v1/task/';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
