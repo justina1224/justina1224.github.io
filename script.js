2ba226759765c96d2019d34aba059717e6d0d953
justina1224 <justyna.pilzno@gmail.com>
2018-10-12 19:15:07 +0200

    updates

--- script.js
+++ script.js
@@ -168,7 +168,7 @@ $(document).ready(function() {
   }
 
   function handleCardCreationRequest(event) {
-    var requestUrl = trelloApiRoot + 'createTrelloCardWithBadges';
+    var requestUrl = trelloApiRoot + 'createTrelloCard';
     var $relatedTaskRow = $(event.target).parents('[data-task-id]');
     var relatedTaskId = $relatedTaskRow.attr('data-task-id');
     var relatedTask = availableTasks[relatedTaskId];
